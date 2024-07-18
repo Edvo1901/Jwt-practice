91ac41983b3bed74adc7a5463582ae225072d746
Edvo1901 <DongVo1901@gmail.com>
2024-07-18 14:31:21 +0930

    login api and checking password

--- backend/src/services/userService.js
+++ backend/src/services/userService.js
@@ -5,14 +5,14 @@ const saltRounds = 10;
 const createUserService = async (name, email, password) => {
 	try {
 		// Hash password
-		const hashPassword = await bcrypt.hash(password, saltRounds)
+		const hashPassword = await bcrypt.hash(password, saltRounds);
 
 		// Save user to database
 		let result = await User.create({
 			name: name,
 			email: email,
 			password: hashPassword,
-			role: "USER", 
+			role: "USER",
 		});
 		return result;
 	} catch (error) {
@@ -21,6 +21,32 @@ const createUserService = async (name, email, password) => {
 	}
 };
 
+const loginUserService = async (email, password) => {
+	try {
+		const user = await User.findOne({ email: email });
+
+		if (user) {
+			const isMatchPassword = await bcrypt.compare(password, user.password);
+
+			if (!isMatchPassword) return { EC: 2, EM: "Invalid password" };
+
+			return {
+				EC: 0,
+				EM: "Success login",
+			};
+		} else {
+			return {
+				EC: 1,
+				EM: "Invalid email/password",
+			};
+		}
+	} catch (error) {
+		console.log(error);
+		return null;
+	}
+};
+
 module.exports = {
 	createUserService,
+	loginUserService,
 };
