b2405bd2c1ef1823648b97abc090801939807d22
Edvo1901 <DongVo1901@gmail.com>
2024-07-18 15:56:47 +0930

    implementing json web token

--- backend/src/services/userService.js
+++ backend/src/services/userService.js
@@ -1,6 +1,8 @@
+require("dotenv").config()
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
+const jwt = require("jsonwebtoken")
 
 const createUserService = async (name, email, password) => {
 	try {
@@ -26,13 +28,34 @@ const loginUserService = async (email, password) => {
 		const user = await User.findOne({ email: email });
 
 		if (user) {
+			// Validate password
 			const isMatchPassword = await bcrypt.compare(password, user.password);
 
 			if (!isMatchPassword) return { EC: 2, EM: "Invalid password" };
 
+			// Create access token
+			const payload = {
+				email: user.email,
+				name: user.name
+			}
+
+			const access_token = jwt.sign(
+				payload,
+				process.env.JWT_SECRET,
+				{
+					expiresIn: process.env.JWT_EXPIRE
+				}
+			)
 			return {
 				EC: 0,
 				EM: "Success login",
+				data: {
+					access_token,
+					user: {
+						email: user.email,
+						name: user.name
+					}
+				}
 			};
 		} else {
 			return {
