5d573e90dc686282ce75c27f9b7d299064ea97be
Edvo1901 <DongVo1901@gmail.com>
2024-07-19 08:51:25 +0930

    setup middleware for backend

--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -1,14 +1,21 @@
 const express = require("express");
-const { createUser, handleLogin, getUsers } = require("../controllers/userController");
+const {
+	createUser,
+	handleLogin,
+	getUsers,
+} = require("../controllers/userController");
+const delay = require("../middleware/delay");
 
 const routerAPI = express.Router();
 
-routerAPI.get("/", (req, res) => {
-	return res.status(200).json("Hello world api")
-})
+routerAPI.all("*", delay);
 
-routerAPI.post("/register", createUser)
-routerAPI.post("/login", handleLogin)
-routerAPI.get("/users", getUsers)
+routerAPI.get("/", (req, res) => {
+	return res.status(200).json("Hello world api");
+});
+
+routerAPI.post("/register", createUser);
+routerAPI.post("/login", handleLogin);
+routerAPI.get("/users", getUsers);
 
 module.exports = routerAPI; //export default
